28d3e244e39e91699e91f547255f44709978afd1
toabr <25079664+toabr@users.noreply.github.com>
2023-03-03 13:09:04 +0100

    nvim: lualine config

--- nvim/.config/nvim/after/plugin/lualine.lua
+++ nvim/.config/nvim/after/plugin/lualine.lua
@@ -7,6 +7,41 @@ if not status_ok then
   return
 end
 
+local components = {
+    lsp = {
+        function()
+            local buf_clients = vim.lsp.buf_get_clients()
+            if next(buf_clients) == nil then
+                return "[inactive...]"
+            end
+
+            local buf_client_names = {}
+            for _, client in pairs(buf_clients) do
+              table.insert(buf_client_names, client.name)
+            end
+
+            local unique_client_names = vim.fn.uniq(buf_client_names)
+            local language_servers = "[" .. table.concat(unique_client_names, ", ") .. "]"
+
+            return language_servers
+        end,
+    },
+    spaces = {
+        function()
+            local shiftwidth = vim.api.nvim_buf_get_option(0, "shiftwidth")
+            return "↦ " .. shiftwidth
+        end,
+        padding = 1,
+    },
+    progress = {
+        "progress",
+        fmt = function()
+            return "%P/%L"
+        end,
+        color = {},
+    },
+}
+
 lualine.setup {
     options = {
         globalstatus = true,
@@ -16,26 +51,23 @@ lualine.setup {
         section_separators = "",
     },
     sections = {
-        lualine_a = {"mode"},
-        lualine_b = {"branch", "diff", "diagnostics"},
-        lualine_c = {
-            { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-            { "filename", path = 1, },
-        },
-        lualine_x = { "encoding", "fileformat", },
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = {},
+        lualine_x = { components.lsp, components.spaces, "filetype" },
         lualine_y = { "location" },
-        lualine_z = { "progress" },
+        lualine_z = { components.progress },
     },
     winbar = {
-        lualine_a = {"filename"},
+        lualine_a = { { "filename", path = 1 } },
         lualine_b = {},
-        lualine_c = {"%="},
+        lualine_c = { "%=" },
         lualine_x = {},
         lualine_y = {},
         lualine_z = {}
     },
     inactive_winbar = {
-        lualine_a = {"filename"},
+        lualine_a = { { "filename" , path = 1 } },
         lualine_b = {},
         lualine_c = {},
         lualine_x = {},
