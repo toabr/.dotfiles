a923ae0766e69604be7494b2b047de1d5dbcb175
toabr <25079664+toabr@users.noreply.github.com>
2024-02-02 12:02:50 +0100

    zsh: fzf location update

--- zsh/.config/zsh/fzf.zsh
+++ zsh/.config/zsh/fzf.zsh
@@ -1,16 +1,16 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == *$HOME/.local/bin/fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}$HOME/.local/bin/fzf/bin"
+if [[ ! "$PATH" == *$HOME/.local/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}$HOME/.local/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$HOME/.local/bin/fzf/shell/completion.zsh" 2> /dev/null
+source "$HOME/.local/fzf/shell/completion.zsh"
 
 # Key bindings
 # ------------
-source "$HOME/.local/bin/fzf/shell/key-bindings.zsh"
+source "$HOME/.local/fzf/shell/key-bindings.zsh"
 
 
 #export FZF_DEFAULT_COMMAND='fdfind --type f --hidden'
