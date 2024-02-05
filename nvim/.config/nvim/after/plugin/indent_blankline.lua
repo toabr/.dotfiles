04f30c80874ed35af3186e0ffd41cc2de5db1349
toabr <25079664+toabr@users.noreply.github.com>
2024-02-05 13:15:40 +0100

    nvim: blankline update

--- nvim/.config/nvim/after/plugin/indent_blankline.lua
+++ nvim/.config/nvim/after/plugin/indent_blankline.lua
@@ -2,17 +2,14 @@
 -- lukas-reineke/indent-blankline.nvim
 --------------------------------------------------------------------------------
 
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
+local status_ok, ibl = pcall(require, "ibl")
 if not status_ok then
   return
 end
 
-indent_blankline.setup {
-    -- for example, context is off by default, use this to turn it on
-    char = '┊',
-    -- show_end_of_line = false,
-    -- show_current_context = true,
-    -- show_current_context_start = false,
-    show_trailing_blankline_indent = false,
+ibl.setup {
+    indent = {
+        char = '┊',
+    }
 }
 
